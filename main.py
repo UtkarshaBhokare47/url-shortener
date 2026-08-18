7f3997827006edbb8f7c58ee5da7ba7990ab75ff
Utkarsha U. Bhokare <utkarshabhokare47@gmail.com>
2026-08-18 18:59:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # ---------------- DATABASE ----------------
 
 def get_db_connection():
-    connection = sqlite3.connect("urls.db")
-    connection.row_factory = sqlite3.Row
+connection = sqlite3.connect("/tmp/urls.db")
+connection.row_factory = sqlite3.Row
     return connection
 
 
